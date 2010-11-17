d8907f099b0877fd5bed6cf0031a482c21f9a3db
aweber <aweber@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-17 14:43:18 +0000

    checking in initial wordpress webform plugin
    git-svn-id: https://plugins.svn.wordpress.org/aweber-web-form-widget/trunk@312532 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ aweber/aweber_forms_import_admin.php
@@ -0,0 +1,113 @@
+<div class="wrap">
+    <h2>AWeber Web Form Options</h2>
+    <form name="aweber_forms_import_form" method="post" action="options.php">
+        <?php wp_nonce_field('update-options'); ?>
+        <input type="hidden" name="aweber_forms_import_hidden" value="Y">
+        <table class="form-table">
+            
+            <?php
+                $pluginAdminOptions = get_option('AWeberWebformPluginAdminOptions');
+                settings_fields('AWeberWebformOauth');
+                $oauth_removed = get_option('aweber_webform_oauth_removed');
+                $oauth_id = get_option('aweber_webform_oauth_id');
+
+                $authorize_success = False;
+                $button_value = 'Make Connection';
+
+                // Check to see if they removed the connection
+                $authorization_removed = False;
+                if ($oauth_removed == 'TRUE') $authorization_removed = True;
+
+                if ($authorization_removed) {
+                    update_option('AWeberWebformPluginAdminOptions', array(
+                        'consumer_key' => '',
+                        'consumer_secret' => '',
+                        'access_key' => '',
+                        'access_secret' => '',
+                    ));
+                    $pluginAdminOptions = get_option('AWeberWebformPluginAdminOptions');
+                    echo '<div id="message" class="updated"><p>Your connection to your AWeber account has been closed.</p></div>';
+                }
+                elseif ($oauth_id and !$pluginAdminOptions['access_secret']) {
+                    // Then they just saved a key and didn't remove anything
+                    // Check it's validity then save it for later use
+                    
+                    try {
+                        list($consumer_key, $consumer_secret, $access_key, $access_secret) = AWeberAPI::getDataFromAweberID($oauth_id);
+                    } catch (AWeberException $e) {
+                        list($consumer_key, $consumer_secret, $access_key, $access_secret) = null;
+                    }
+                    if (!$access_secret) {
+                        echo $this->message['access_token_failed'];
+                    } else {
+                        update_option('AWeberWebformPluginAdminOptions', array(
+                            'consumer_key' => $consumer_key,
+                            'consumer_secret' => $consumer_secret,
+                            'access_key' => $access_key,
+                            'access_secret' => $access_secret,
+                        ));
+                    }
+                    $pluginAdminOptions = get_option('AWeberWebformPluginAdminOptions');
+                }
+                if ($pluginAdminOptions['access_key']) {
+                    extract($pluginAdminOptions);
+                    $aweber = $this->_get_aweber_api($consumer_key, $consumer_secret);
+                    try {
+                        $account = $aweber->getAccount($access_key, $access_secret);
+                    } catch (AWeberResponseError $e) {
+                        $account = null;
+                    }
+                    
+                    if (!$account) {
+                        echo $this->message['auth_failed'];
+                    }
+                    
+                    $authorize_success = True;
+                    $button_value = 'Remove Connection';
+                } 
+
+                // Checks to see if the widget is installed
+                // Not used yet, waiting on wireframe
+                $aweber_option_name = strtolower($this->widgetOptionsName);
+                $installed_widget = false;
+
+                foreach (get_option('sidebars_widgets') as $widget){
+                    if (is_array($widget) and in_array($aweber_option_name, $widget)) {
+                        $installed_widget = true;
+                    } 
+                }
+            ?>
+            <?php if ($authorize_success): 
+            ?>
+
+            <p>You've successfully connected to your AWeber account!</p>
+            <p>Next step - go to the <a href="widgets.php">Widgets Page</a> and drag the AWeber Web Form widget into your widget area.</p>
+                <input type="hidden" name="aweber_webform_oauth_removed" value="TRUE" />
+            <?php else: ?>
+                <tr valign="top">
+                <th scope="row">Step 1:</th>
+                <td><a target="_blank" 
+                       href="https://auth.aweber.com/1.0/oauth/authorize_app/f49b1bcf">Click here to get your authorization code</a>.
+                </tr>
+
+                <tr valign="top">
+                <th scope="row">Step 2: Paste in your authorization code:</th>
+                <td><input type="text" name="aweber_webform_oauth_id"
+                           value="<?php echo $oauth_id; ?>" /></td>
+            <?php endif ?>
+            <?php if ($authorization_removed or $authorize_success): ?>
+                <script type="text/javascript" >
+                    jQuery.noConflict();
+                    jQuery("#setting-error-settings_updated").hide();
+                    jQuery("#aweber_auth_error").hide();
+                </script>
+            <?php endif ?>
+        <input type="hidden" name="action" value="update" />
+        <input type="hidden" name="page_options" value="aweber_webform_oauth_id" />
+        </table>
+
+                <p class="submit">
+                    <input type="submit" class="button-primary" value="<?php _e($button_value) ?>" />
+                </p>
+    </form>
+</div>
---
+++ aweber/readme.txt
@@ -0,0 +1,92 @@
+=== AWeber Web Form Plugin ===
+Contributors: aweber
+Donate link: http://www.aweber.com
+Tags: email, marketing, email marketing, webform, web, form, aweber, mailing list, API, newsletter
+Requires at least: 2.7.0
+Tested up to: 3.0.1
+Stable tag: 1.0
+
+This widget allows you to quickly and easily install an AWeber web form on your Wordpress blog using AWeber's API.
+
+== Description ==
+
+Drag and drop web forms that you have created in your <a href="http://www.aweber.com" title="AWeber email marketing">AWeber email marketing</a> account into your blog, without having to log into your AWeber account or copy and paste anything! All you need is an AWeber account and a <a href="http://www.aweber.com/faq/questions/53">completed web form</a>.
+
+= Learn More About AWeber =
+
+<a href="http://www.aweber.com/">AWeber</a> is an email marketing service that helps users to stay in contact with their website visitors, blog readers and customers. Take a look at <a href="http://www.aweber.com/email-marketing-features.htm">what AWeber has to offer</a> and get started with a brand new account for <a href="https://www.aweber.com/order.htm">just a dollar</a>.
+
+= How Was This Widget Made? =
+
+This widget was created using AWeber's API. To learn more about the API and create your own widgets and apps, visit <a href="http://labs.aweber.com">AWeber Labs</a>.
+
+== Installation ==
+
+If you have any trouble following the instructions below, check the Screenshots tab for a visual step-by-step.
+
+If you're manually installing the widget as a .zip, make sure you upload it to "/wp-content/plugins/". Once installed, activate it on your Plugins page by clicking the Activate link.
+
+To connect the widget to your AWeber account, first click AWeber Web Form under the Settings tab on the left of your Wordpress control panel.
+
+On this page, click the link next to "Step 1".
+
+You will be taken to a page asking you to authorize the connection between your AWeber account and the widget. Enter your login name and password here and hit the "Allow" button.
+
+You'll be taken to a page with an authorization code (it will be a long string of characters) - simply highlight it and copy it to your clipboard.
+
+Back in your Wordpress account, just paste the authorization code into the space provided and click the "Make Connection" button.
+
+Once you've installed this widget and connected it to your AWeber account, head over to the Widgets page under Appearance on the left side of your Wordpress dashboard.
+
+Simply drag the widget (it will show up under Available Widgets as "AWeber Web Form") into the widget area in which you'd like your form to appear.
+
+Once the widget is in place, you can choose which form you would like to appear. Expand the AWeber Web Form widget's options by clicking the arrow to its right, then use the drop down menus to choose the list you want to work with, and the web form you'd like to place on your blog.
+
+Then hit the "Save" button to save your changes, and your form will be installed on your blog.
+
+= Having Trouble? =
+
+If you don't see any available web forms, make sure you've selected the right list.
+
+If you're in the list you want subscribers added to, but still don't see a web form that you can add, you'll need to <a href="https://www.aweber.com/login.htm">log in</a> to your AWeber account and <a href="http://www.aweber.com/faq/questions/53">create a web form</a> first.
+
+== Frequently Asked Questions ==
+
+= How Do I Create a Web Form? =
+
+For a step by step walkthrough on creating a web form, <a href="http://www.aweber.com/faq/questions/53">consult our knowledge base</a>.
+
+= My Form Doesn't Fit in the Sidebar - How Do I Fix It? =
+
+Wider forms may not fit perfectly at first, depending on the theme you are using for your blog. Luckily, you can easily resize your form in AWeber's web form generator.
+
+<a href="http://www.aweber.com/faq/questions/336/How+Can+I+Resize+My+Form%3F">Take a look at our knowledge base for a tutorial</a>.
+
+= I Created a Web Form, But I Can't Find it in the AWeber Web Form Widget. =
+
+If you just created the web form, try refreshing the page and looking again.
+
+Also, check any other lists that appear in the first drop down in the Web Form widget - it's important to make sure that you're working in the list you want subscribers to be added to.
+
+== Screenshots ==
+
+1.
+2.
+3.
+4.
+5.
+6.
+7.
+8.
+9.
+10.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+ * Initial release.
